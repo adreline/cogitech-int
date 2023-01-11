95fd37ace107da4ee4b4f014cc482decb63527e3
adreline <maddie_madlad@mailbox.org>
2023-01-11 17:48:45 +0100

    user authentication added

--- migrations/Version20230111161902.php
+++ migrations/Version20230111161902.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230111125307 extends AbstractMigration
+final class Version20230111161902 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,15 @@ final class Version20230111125307 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE post (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, body CLOB DEFAULT NULL)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE user');
     }
 }
